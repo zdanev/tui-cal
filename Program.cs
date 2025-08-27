3f63c30871cc129854b2777c50e6648d10c3c749
Zdravko Danev <zdanev@gmail.com>
2025-08-26 19:17:38 -0700

    added Terminal.Gui and created main form

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Terminal.Gui;
+
+Application.Init();
+var top = Application.Top;
+
+// Creates the top-level window to show
+var win = new Window("TUI Calendar")
+{
+    X = 0,
+    Y = 1, // Leave one row for the toplevel menu
+
+    // By using Dim.Fill(), it will automatically resize without manual intervention
+    Width = Dim.Fill(),
+    Height = Dim.Fill()
+};
+
+top.Add(win);
+Application.Run();
